faeba8d0cc0d71a5e4ce4a202b36e51f8a93f446
sumashiva <sumareddy.xworkz@gmail.com>
2021-10-02 14:54:26 +0530

    firstprogram

---
+++ WatchStarter.java
@@ -0,0 +1,21 @@
+class WatchStarter
+{
+public static void main(String[] kichdis)
+{
+//primitive=initializarion
+//non primitive=instanstation
+Watch watch=new Watch(467,"rolex");
+System.out.println(watch.model);
+System.out.println(watch.brand);
+int model=watch.model;
+String brand=watch.brand;
+Light light=watch.light;
+System.out.println(light.on);
+}
+
+
+}
+
+
+
+
