1ad44eb10eab3738796caa7052c350bed82d3755
sumashiva <sumareddy.xworkz@gmail.com>
2021-10-02 19:02:25 +0530

    second program

---
+++ Watch.java
@@ -0,0 +1,15 @@
+class Watch
+{
+int model=478;
+String brand="rolex";
+Light light=new Light();
+
+Watch(int model,String brand)
+{
+this.model=model;
+this.brand=brand;
+System.out.println("invoked int and string const");
+
+}
+
+}
